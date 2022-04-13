eb2891ecec94b410bda427764ebebf27a9676c2e
rezazakernrjad <102189337+rezazakernrjad@users.noreply.github.com>
2022-04-13 13:48:03 +0200

    template function to compare shapes
    re-commit wrongly overwritten template function to compare shapes

--- week3/class_template_overload.cpp
+++ week3/class_template_overload.cpp
@@ -5,34 +5,56 @@ prints out the result.
 
 #include "class_template_overload.hpp"
 
-int Circle::Area(){
-    return (this->diameter_/2)* (this->diameter_/2) * 3;}
-int Circle::Perimeter(){
-    return 3 * diameter_; }
-int Rectangle::Area(){
-    return height_ * width_;}
-int Rectangle::Perimeter(){
-     return 2* (height_ + width_);}
-int Triangle::Area(){
-     return (sqrt(3)/2) * side_;}
-int Triangle::Perimeter(){ 
-      return side_ *3;
-  }
+int Circle::Area()
+{
+    return (this->diameter_ / 2) * (this->diameter_ / 2) * 3;
+}
+int Circle::Perimeter()
+{
+    return 3 * diameter_;
+}
+int Rectangle::Area()
+{
+    return height_ * width_;
+}
+int Rectangle::Perimeter()
+{
+    return 2 * (height_ + width_);
+}
+int Triangle::Area()
+{
+    return (sqrt(3) / 2) * side_;
+}
+int Triangle::Perimeter()
+{
+    return side_ * 3;
+}
 
-int main(){
+int main()
+{
     cout << "Shape...::::" << endl;
 
-    Circle* cirk = new Circle(3);
-    Rectangle* rect = new Rectangle(3,5);
-    Triangle* tri = new Triangle(5);
+    Circle *cirk = new Circle(3);
+    Rectangle *rect = new Rectangle(3, 5);
+    Triangle *tri = new Triangle(5);
     bool shape_comp = ShapeCompare<Circle, Triangle>(cirk, tri);
-    if (shape_comp){
-        cout << "Circle and Triangle are equals" << endl;    
-    } else {cout << "Circle and Triangle are NOT equals" << endl;}
+    if (shape_comp)
+    {
+        cout << "Circle and Triangle are equals" << endl;
+    }
+    else
+    {
+        cout << "Circle and Triangle are NOT equals" << endl;
+    }
 
     shape_comp = ShapeCompare<Triangle, Rectangle>(tri, rect);
-    if (shape_comp){
-        cout << "Triangle and Rectangle are equals" << endl;    
-    } else {cout << "Triangle and Rectangle are NOT equals" << endl;}
+    if (shape_comp)
+    {
+        cout << "Triangle and Rectangle are equals" << endl;
+    }
+    else
+    {
+        cout << "Triangle and Rectangle are NOT equals" << endl;
+    }
     return 0;
 }
